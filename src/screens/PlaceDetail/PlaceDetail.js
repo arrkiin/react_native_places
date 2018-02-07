ea2df6c689352b98ee194bfa58fa352f8c092b28
arrkiin <arrkiin@gmail.com>
2018-02-07 15:13:31 +0100

    Working pop after delete place

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     View,
     Image,
@@ -8,25 +8,35 @@ import {
     TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
 
-const placeDetail = props => {
-    return (
-        <View style={styles.container}>
-            <Image
-                source={props.selectedPlace.image}
-                style={styles.placeImage}
-            />
-            <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
-            <View>
-                <TouchableOpacity onPress={props.onItemDeleted}>
-                    <View style={styles.deleteButton}>
-                        <Icon name="ios-trash" size={30} color="red" />
-                    </View>
-                </TouchableOpacity>
+class PlaceDetail extends Component {
+    placeDeletedHandler = () => {
+        this.props.onDeletePlace(this.props.selectedPlace.key);
+        this.props.navigator.pop();
+    };
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={this.props.selectedPlace.image}
+                    style={styles.placeImage}
+                />
+                <Text style={styles.placeName}>
+                    {this.props.selectedPlace.name}
+                </Text>
+                <View>
+                    <TouchableOpacity onPress={this.placeDeletedHandler}>
+                        <View style={styles.deleteButton}>
+                            <Icon name="ios-trash" size={30} color="red" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
-    );
-};
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -46,4 +56,10 @@ const styles = StyleSheet.create({
     },
 });
 
-export default placeDetail;
+const mapDispatchToProps = dispatch => {
+    return {
+        onDeletePlace: key => dispatch(actions.deletePlace(key)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(PlaceDetail);
