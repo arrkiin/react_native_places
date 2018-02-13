7e08572476e237bb5bebd7f6c3223c9c6d462e7c
arrkiin <arrkiin@gmail.com>
2018-02-13 16:32:05 +0100

    Start integrating firebase and storage

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,30 @@
 import * as actionTypes from './actionTypes';
 
+// const addPlaceAction = (placeName, location, image) => {
+//     type: actionTypes.ADD_PLACE,
+//     placeName: placeName,
+//     location: location,
+//     image: image,
+// }
+
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: actionTypes.ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image,
+    return dispatch => {
+        const placeData = {
+            name: placeName,
+            location: location,
+        };
+        // fetch(
+        //     'https://awesome-places-1518441622081.firebaseio.com/places.json',
+        //     {
+        //         method: 'POST',
+        //         body: JSON.stringify(placeData),
+        //     }
+        // )
+        //     .catch(err => console.log(err))
+        //     .then(res => res.json())
+        //     .then(parsedResp => {
+        //         console.log(parsedResp);
+        //     });
     };
 };
 
